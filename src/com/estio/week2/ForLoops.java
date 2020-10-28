93f1706e72daf8aa043bb194342422451bad68d4
Idris Fabiyi <idris.fabiyi@estio.co.uk>
2020-10-28 14:27:41 +0000

    Class code

--- src/com/estio/week2/ForLoops.java
+++ src/com/estio/week2/ForLoops.java
@@ -1,5 +1,8 @@
 package com.estio.week2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ForLoops
 {
     /**
@@ -9,7 +12,7 @@ public class ForLoops
     public static void main(String[] args)
     {
         // print out 1 to 10 using a for loop
-        for(int x = 0; x <= 10 ; x+=3 )
+        /*for(int x = 0; x <= 10 ; x+=3 )
         {
 
             if(x > 1 && x < 10)
@@ -20,9 +23,58 @@ public class ForLoops
             {
                 System.out.print(x + ".");
             }
-        }
+        }*/
+
+//        numbersTable();
+
+        forEachExample();
 
     }
+
+    public static void forEachExample()
+    {
+/*        List<String> names = new ArrayList<String>();
+        names.add("Shelly");
+        names.add("Josh");
+        names.add("Tyler");
+        names.add("Matt");
+
+        // print out all names
+        for(String name : names)
+        {
+            System.out.println(name);
+        }*/
+
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        String[] names = {"Tyler", "Will", "Shelly", "Josh"};
+        System.out.println(names.length);
+
+        for(int x = 0; x < names.length; x++)
+        {
+            System.out.println( names[x] );
+        }
+
+
+
+        int sum =0;
+        // add 5 numbers to the list
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(5);
+        numbers.add(5);
+
+        for (Integer number: numbers )
+        {
+            sum += number;
+        }
+
+        System.out.println("The sum of the numbers is:" + sum);
+
+
+   }
+
 
     /**
      * This method will give us some examples of using a for loop for
@@ -38,16 +90,21 @@ public class ForLoops
 
     public static void numbersTable()
     {
+
         for (int x = 1 ; x <= 10 ; x++)
         {
             System.out.println(x);
-            // create another loop
-            for (int y = 0; y <= 10 ; y++)
-            {
-                System.out.println(x + "\t"+ y + "\t" );
-            }
-
+                for(int y=1 ; y<=10 ; y++)
+                {
+                    System.out.println("X: " + x + "Y:" + y);
+                }
+            System.out.println();
         }
+    }
+
+    public static void extraLoopTask()
+    {
+
     }
 
 
---
+++ src/com/estio/week2/School.java
@@ -0,0 +1,71 @@
+package com.estio.week2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class School
+{
+    public static void main(String[] args)
+    {
+        menu();
+    }
+
+    public static void menu()
+    {
+        boolean isProgramEnded = false;
+        Scanner userInput = new Scanner(System.in);
+
+        int choice = 0;
+
+        System.out.println("" +
+                "What would you like to do\n" +
+                "\tPress 1 for ...\n"+
+                "\tPress 2 for ...\n"+
+                "\tPress 3 for ...\n"+
+                "\t123 to quit"+
+                "");
+
+        try
+        {
+            choice = userInput.nextInt();
+        }
+        catch (InputMismatchException ex)
+        {
+            System.out.println("Error option selected");
+        }
+
+        if(choice == 123)
+        {
+            // quit application
+            System.exit(0);
+        }
+        else
+        {
+            while(!isProgramEnded)
+            {
+                if(choice == 1)
+                {
+                    // do what 1 does
+                    System.out.println("Option 1 selected");
+                    menu();
+                }
+                else if (choice == 2)
+                {
+                    System.out.println("Option 2 selected");
+                    menu();
+
+                }
+                else if (choice == 3)
+                {
+                    System.out.println("Option 3 selected");
+                    menu();
+                }
+                else
+                {
+                    System.out.println("Enter a valid choice");
+                    menu();
+                }
+            }
+        }
+    }
+}
