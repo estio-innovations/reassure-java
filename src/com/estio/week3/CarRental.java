ff76e9d245e2644041f294080facac08c4883694
Idris Fabiyi <idris.fabiyi@estio.co.uk>
2020-11-12 14:07:28 +0000

    Lease Car Task Part 2

--- src/com/estio/week3/CarRental.java
+++ src/com/estio/week3/CarRental.java
@@ -2,17 +2,59 @@ package com.estio.week3;
 import com.estio.week3.carrental.LeaseCar;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-
+/***
+ * You might choose to complete this class by finishing off the options within the menu method
+ * Use the hertz ArrayList to update the list with new lease cars
+ *
+ * Implement options to add the check the availability of leased cars, lease a car, check remove and even editing of leasing details.
+ */
 public class CarRental
 {
     LeaseCar bmw;
     LeaseCar citron;
-    LeaseCar fait;
+    LeaseCar fiat;
+    ArrayList<LeaseCar> hertz = new ArrayList<LeaseCar>();
 
     public CarRental()
     {
         setupCarsInit();
+    }
+
+    public void showMenu()
+    {
+        Scanner userIn = new Scanner(System.in);
+        boolean isProgramRunning = true;
+
+        do {
+            System.out.println("What whould you like to do?");
+            System.out.println("1: display all cars");
+            System.out.println("2: add a new car for lease");
+            System.out.println("3: Lease a new car");
+            System.out.println("9: Exit program");
+            Integer choice = userIn.nextInt();
+            if(choice == 1)
+            {
+
+            }
+            else if(choice == 2)
+            {
+                System.out.println("Displaying all details on cars");
+                printAllCarDetails();
+            }
+            else if(choice == 2)
+            {
+                System.out.println("add a new car for lease");
+            }
+            else
+            {
+                System.out.println();
+            }
+
+
+        }while (isProgramRunning);
     }
 
     public static void main(String[] args)
@@ -22,16 +64,21 @@ public class CarRental
 
     public void printAllCarDetails()
     {
-        System.out.println(bmw.describeLeasedCar());;
-        System.out.println(citron.describeLeasedCar());;
-        System.out.println(fait.describeLeasedCar());;
+        // loop around and get all details
+        for(LeaseCar car: hertz)
+        {
+            System.out.println(car.describeLeasedCar());
+        }
     }
 
     private void setupCarsInit()
     {
         bmw = new LeaseCar("BMW M5", false, LocalDate.now(), LocalDate.now().plusDays(120));
         citron = new LeaseCar("Picasa", false, LocalDate.now(), LocalDate.now().plusDays(120));
-        fait = new LeaseCar("Panda", true, LocalDate.now(), LocalDate.now().plusDays(120));
+        fiat = new LeaseCar("Panda", true, LocalDate.now(), LocalDate.now().plusDays(120));
+        hertz.add(bmw);
+        hertz.add(citron);
+        hertz.add(fiat);
         printAllCarDetails();
     }
 
